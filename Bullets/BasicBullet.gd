10648aa6e8ca9358f7b4e2c54029f7a5e3bc6375
Oli Brock <kbshields@gmail.com>
2021-03-13 22:59:32 -0500

    Strafing and flying have different powers, dropped friction way down,  improved bullet look and feel

--- Bullets/BasicBullet.gd
+++ Bullets/BasicBullet.gd
@@ -8,15 +8,19 @@ const Damage = 10
 var originalPos = Vector2.ZERO
 
 onready var animPlayer = $AnimationPlayer
+onready var sprite = $Sprite
 
 
 func launchBullet(rnd_impulse, direction):
 	originalPos = self.position	
 	
 	apply_central_impulse(direction * (ImpulseMag * rnd_impulse))
+	
 
 func _physics_process(delta):
 	var distanceTravelled = self.position.distance_to(originalPos)
+	if distanceTravelled > 0:
+		sprite.visible = true;
 	if distanceTravelled > MaxDistance:
 		queue_free()
 		
