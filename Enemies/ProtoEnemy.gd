e2242a47ec02b776ddb1ca34f27f726684a05b6d
Oli Brock <kbshields@gmail.com>
2021-03-17 19:44:47 -0400

    Very basic enemy "AI" to track the player and leash when the player gets far enough away using 2 different Area2D detection zones

---
+++ Enemies/ProtoEnemy.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var target = null
+var chase_speed = 50
+var velocity = Vector2.ZERO
+
+
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	if target:
+		look_at(target.position)
+		velocity = position.direction_to(target.position) * chase_speed
+	velocity = move_and_slide(velocity)
+
+func _on_PlayerDetect_body_entered(body):
+	if body.is_in_group("player"):
+		target = body
+
+
+
+
+func _on_PlayerLeash_body_exited(body):
+	if body.is_in_group("player"):
+		target = null
