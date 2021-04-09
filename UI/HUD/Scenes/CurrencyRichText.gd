77b624af8cc855d88b53c8135bd060b9a213699b
brianjboucher <brianb823@gmail.com>
2021-04-09 17:10:17 -0500

    ui (hud) simplified CurrencyRichText.gd

--- UI/HUD/Scenes/CurrencyRichText.gd
+++ UI/HUD/Scenes/CurrencyRichText.gd
@@ -1,9 +1,7 @@
 extends RichTextLabel
 
-onready var playerVars = get_node("/root/PlayerVars")
-
 func _ready():
 	pass # Replace with function body.
 
-func _process(delta):
-	get_node("/root/World/CanvasLayer/HUD/HudVbox/CurrencyHBox/CurrencyRichText").bbcode_text = "[color=yellow]$[/color]" + String(playerVars.player["cash"])
+func _process(_delta):
+	self.bbcode_text = "[color=yellow]$[/color]" + String(PlayerVars.player["cash"])
