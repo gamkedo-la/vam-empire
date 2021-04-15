c114ea0c7321ae207743a0e3e3be3b3e8f2219b1
Oli Brock <kbshields@gmail.com>
2021-04-15 12:33:19 -0400

    feat (Music): is now in game as a persistent background scene, volume is eased in over 4 seconds at load

---
+++ Scripts/Autoload/Music.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+onready var ambient_explo_music = $Ambient_Exploration
+onready var tween = $Tween
+
+func _ready():
+	# Ease the music in to avoid 'startling' the player on game load
+	#Option #1 For Easing options check out http://easings.net
+	tween.interpolate_property(
+		ambient_explo_music,
+		"volume_db",
+		ambient_explo_music.volume_db, 0, 4,Tween.TRANS_QUART, Tween.EASE_IN_OUT)
+	tween.start()
+	
+	# Option #2 (uncomment and comment above block)
+#	ambient_explo_music.volume_db = 0
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
