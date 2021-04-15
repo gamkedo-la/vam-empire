30059daf5b11a6b360223c9c1fb205913ebcf5e8
Oli Brock <kbshields@gmail.com>
2021-04-15 12:42:45 -0400

    fix (StartMenu) now flys around again. on_Tween_completed accidentally got disconnected on the menu rework

--- Scripts/Autoload/Music.gd
+++ Scripts/Autoload/Music.gd
@@ -4,13 +4,15 @@ extends Node
 onready var ambient_explo_music = $Ambient_Exploration
 onready var tween = $Tween
 
+var easein_time = 4
+
 func _ready():
 	# Ease the music in to avoid 'startling' the player on game load
 	#Option #1 For Easing options check out http://easings.net
 	tween.interpolate_property(
 		ambient_explo_music,
 		"volume_db",
-		ambient_explo_music.volume_db, 0, 4,Tween.TRANS_QUART, Tween.EASE_IN_OUT)
+		ambient_explo_music.volume_db, 0, easein_time,Tween.TRANS_QUART, Tween.EASE_IN_OUT)
 	tween.start()
 	
 	# Option #2 (uncomment and comment above block)
