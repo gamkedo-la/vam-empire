ecac5d680dd2c9673a49b28dcb0aff31ce145919
Derek Miranda <derek.miranda.1992@gmail.com>
2021-04-22 18:29:29 -0700

    feat(Items): start of item collection implementation
    - sets Circle collision shape on Item scene
    - player colliding w/ item triggers collection

--- Scripts/Database/Item.gd
+++ Scripts/Database/Item.gd
@@ -9,8 +9,17 @@ export (Texture) var itemTexture
 
 
 
-func _init(itemName:String, itemType:int, asteroids:Array, itemTexture:Texture):
+func _init(itemName:String = "", itemType:int = 0, asteroids:Array = [], itemTexture:Texture = null):
 	pass
 
 func _ready():
 	print(Database.ItemType)
+
+func collect():
+	# TODO: update inventory
+	print("Collected ", itemName)
+	queue_free()
+
+
+func _on_LootTrigger_area_entered(area):
+	collect()
