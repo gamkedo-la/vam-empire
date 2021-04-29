52410b006fb32d41ef864aec4e8c9c756d00bcf2
Derek Miranda <derek.miranda.1992@gmail.com>
2021-04-28 18:34:14 -0700

    feature(MiningLaser): mining laser continuously applies damage
    - done w/ timer and toggling hurtBox.monitorable

--- Scripts/MiningLaserBeam.gd
+++ Scripts/MiningLaserBeam.gd
@@ -5,10 +5,14 @@ const MaxSeconds = 1
 const ImpulseMag = 550
 const Damage = 100
 
+export (float) var tickTime = 0.3
+
 var originalPos = Vector2.ZERO
 
 onready var animPlayer = $AnimationPlayer
 onready var sprite = $Sprite
+onready var damageTickTimer = $DamageTickTimer
+onready var hurtBox = $HurtBox
 
 func _ready():
 	animPlayer.play("fireLaser")
@@ -17,6 +21,18 @@ func _ready():
 
 func hit_something():
 	animPlayer.play("keepFiringLaser")
+	disable_collision_temporarily()
+	
+func disable_collision_temporarily():
+	disable_collision()
+	damageTickTimer.start(tickTime)
+	
+func disable_collision():
+	hurtBox.set_deferred("monitorable", false)
 	
 func free_laser_beam():
 	queue_free()
+
+# e.g. when temporary laser disabling is over
+func _on_DamageTickTimer_timeout():
+	hurtBox.monitorable = true
