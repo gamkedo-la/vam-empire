c398a50fb7bc0bbed353e4133242418b6cca627f
KornelGM <KORNik_k@wp.pl>
2021-06-20 11:31:36 +0200

    Custom cursors test, working but not used in-game

---
+++ Scripts/Autoload/Cursor.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var normal = load("res://Cursors/normal.png")
+var overUI = load("res://Cursors/overUI.png")
+var target = load("res://Cursors/target.png")
+
+func _ready():
+	set_default()
+
+func set_default():
+	Input.set_custom_mouse_cursor(normal)
+
+func set_over_active_ui_element():
+	Input.set_custom_mouse_cursor(overUI)
+
+func set_target():
+	Input.set_custom_mouse_cursor(target)
