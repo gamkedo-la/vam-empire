39ed1cb196b7dc7fe788a811401d5700ffb4347c
Oli Brock <kbshields@gmail.com>
2021-07-08 15:54:27 -0400

    tweak (Expedition) missions always show on map and icon is removed when mission completed

--- Scripts/ZoneOrchestration/MissionNodes/Expedition.gd
+++ Scripts/ZoneOrchestration/MissionNodes/Expedition.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal removed
+
 export (String) var mission_string = null
 onready var miner_detect: Area2D = $Checkpoint/CheckpointDetect
 var visited = false
@@ -15,4 +17,5 @@ func _check_player_visited(_body: Node) -> void:
 		if mission_string && !visited:
 			print_debug("mission_string exists ", mission_string)
 			PlayerVars.emit_signal("checkpoint_reached", mission_string)
+			emit_signal("removed", self)
 			visited = true
