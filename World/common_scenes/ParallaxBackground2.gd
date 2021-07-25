550c2d519383280700c922737e260fd4bb452eb8
Oli Brock <kbshields@gmail.com>
2021-07-25 17:59:13 -0400

    art (Expedition, Planet) sprites added for missions and background

---
+++ World/common_scenes/ParallaxBackground2.gd
@@ -0,0 +1,16 @@
+extends ParallaxBackground
+
+onready var starfield = $StarFieldBlack/Sprite
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(_delta) -> void:
+	
+	pass
